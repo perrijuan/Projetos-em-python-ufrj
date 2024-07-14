217ee28df2e9df07e72dcc3eb47d80fa27465ef9
Juan perri <111884460+Xorjuan@users.noreply.github.com>
2024-07-14 01:29:54 -0300

    Create prova p1.py
    converte de base 10 para bin e hex

---
+++ prova p1.py
@@ -0,0 +1,13 @@
+# questão 1
+def convert_hex(hex):
+    print(int(hex, 16))  # converte de str para int em base 16
+
+
+def convert_bin(binario):
+    print(bin(binario))  # converte em int -> bin
+
+
+convert_hex(str(input("digite um valor para a conversão em hexa:")))
+convert_bin(int(input("digite um valor para a conversão em binario:")))
+
+# questão 2
